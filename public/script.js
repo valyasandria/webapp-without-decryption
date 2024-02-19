3ed9407ddfd540e3a6ddf8a4aff6b4771c615de6
valyasandria <valya.sandria@ui.ac.id>
2024-02-19 15:02:26 +0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,104 @@
+// backend 
+
+// custom allert in login page
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.getElementById('loginButton');
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+
+    const customAlert = document.getElementById('customAlert');
+    const alertMessage = document.getElementById('alertMessage');
+    const closeAlertButton = document.getElementById('closeAlert');
+
+    // Function to show custom alert
+    function showAlert(message) {
+        alertMessage.textContent = message;
+        customAlert.style.display = 'block';
+    }
+
+    // Close the custom alert box
+    closeAlertButton.addEventListener('click', function() {
+        customAlert.style.display = 'none';
+    });
+
+//LOGIN 
+loginButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    if (username.value && password.value) {
+        // Send POST request to server
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+        },
+            body: JSON.stringify({
+                username: username.value,
+                password: password.value
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    window.location.href = '/home.html';
+                } else {
+                    showAlert(data.message || 'Incorrect username or password');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                showAlert('An error occurred. Please try again later.');
+            });
+        } else {
+            // If one or both fields are empty, alert the user
+            showAlert('Please enter both username and password');
+        }
+    });
+});
+
+//HOME
+document.addEventListener('DOMContentLoaded', function() {
+    const suhuElement = document.getElementById('suhu-value');
+    const kipasElement = document.getElementById('kipas-status');
+
+    function updateSuhuDanKipas() {
+        fetch('/getData')
+            .then(response => response.json())
+            .then(data=>{
+                const suhu = data.decryptedData
+                suhuElement.textContent = `${parseFloat(suhu).toFixed(2)}°C`;
+                // Mengubah kondisi kipas berdasarkan suhu
+                if (suhu >= 30) {
+                    kipasElement.textContent = `ON`;
+                } else {
+                    kipasElement.textContent = `OFF`;
+                }
+            })
+    }
+
+    setInterval(updateSuhuDanKipas, 60000); // Perbarui suhu dan status kipas setiap 60 detik
+});
+
+//LOGOUT
+document.addEventListener('DOMContentLoaded', function() {
+    const logoutButton = document.getElementById('logoutButton');
+
+    logoutButton.addEventListener('click', function() {
+        // Send a request to the server's logout endpoint
+        fetch('/logout', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Redirect to login page after successful logout
+            window.location.href = 'login.html';
+        })
+        .catch(error => {
+            console.error('Error during logout:', error);
+        });
+    });
+});
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,191 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const crypto = require('crypto');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const path = require('path');
+
+// express.static middleware to serve static files from 'public' directory
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Configure session middleware
+app.use(session({
+  secret: 'mykeysession', // Replace with your secret key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto', httpOnly: true } // Set to true if using https
+}));
+
+// PostgreSQL connection settings
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+  });
+  
+// ENDPOINT INSERT TEMPERATURE TO DATABASE
+app.post('/temperature_endpoint', async (req, res) => {
+  try {
+    const encryptedTemperature = req.body.data;
+  
+    console.log("Received temperature:", encryptedTemperature);
+  
+    // Insert data into PostgreSQL
+    const query = 'INSERT INTO temperature_plain (temperature) VALUES ($1)';
+    await pool.query(query, [encryptedTemperature]);
+  
+    res.send('Data received and stored.');
+  } catch (error) {
+      console.error('Database operation failed:', error);
+      res.status(500).send('Server error');
+    }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html')); // Send file HTML as response
+});
+
+// ENDPOINT LOGIN
+app.post('/login', function(req, res) {
+  // Extract username and password from req.body
+  const { username, password } = req.body;
+  
+  // validasi kredensial
+  if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+      req.session.user = { username: username };
+      res.json({ success: true });
+  } else {
+      res.json({ success: false, message: 'Incorrect username or password' });
+  }
+});
+
+// ENDPOINT LOGOUT
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            // handle error
+            res.status(500).send('Could not log out');
+        } else {
+            res.clearCookie('connect.sid');
+            res.json({ message: 'Logged out successfully' });
+        }
+    });
+});
+
+// Middleware to check if the user is logged in
+function checkAuthenticated(req, res, next) {
+  if (req.session.user) {
+      next();
+  } else {
+      res.redirect('/login');
+  }
+}
+
+// Middleware to check if the user is already logged in
+function checkNotAuthenticated(req, res, next) {
+  if (req.session.user) {
+      res.redirect('/home');
+  } else {
+      next();
+  }
+}
+
+// Home page route
+app.get('/home', checkAuthenticated, (req, res) => {
+  res.send('Welcome to the Home Page');
+});
+
+// Login page route
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.send('Login Here');
+});
+
+// Function to decrypt data
+function decrypt(encryptedData) {
+  // Key dan IV harus sama dengan yang di ESP32
+  const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+  const iv = Buffer.from(process.env.IV, 'hex');
+
+  // Konversi hex string dari data suhu di database ke Buffer
+  const encrypted = Buffer.from(encryptedData, 'hex');
+  console.log('encrypted : ', encryptedData)
+  try{
+    // Inisialisasi dechipher
+    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+    decipher.setAutoPadding(true); // Aktifkan auto-padding untuk menghandle PKCS#7 padding
+    let decrypted = decipher.update(encrypted, null, 'utf8');
+    decrypted += decipher.final('utf8');
+
+    console.log('decryted: ', decrypted)
+    return decrypted;
+
+  } catch (error) {
+    console.error('Decrypt Error:', error.message);
+    return null;
+  }
+}
+
+// ENDPOINT RETRIEVE LATEST DATA FROM DB
+app.get('/getData', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT temperature FROM temperature_plain ORDER BY id DESC LIMIT 1');
+    if (result.rows.length > 0) {
+      // Mendekripsi data temperature
+      const plainData = result.rows[0].temperature;
+      console.log('fetched: ', plainData)
+      res.json({ Data: plainData });
+    } else {
+      // Mengirimkan respons jika tidak ada data yang ditemukan
+      res.status(404).send('No temperature data found');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+//======================== TESTING =========================
+// Fungsi untuk enkripsi data
+function encrypt(data) {
+  const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+  let encrypted = cipher.update(data, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+}
+
+
+// Endpoint untuk enkripsi data
+app.post('/encryptData', (req, res) => {
+  const { data } = req.body;
+  if (data) {
+    const encryptedData = encrypt(data);
+    res.json({ encryptedData });
+  } else {
+    res.status(400).send('No data provided');
+  }
+});
+
+// Endpoint untuk mendekripsi data
+app.post('/decryptData', (req, res) => {
+  const { encryptedData } = req.body;
+  if (encryptedData) {
+    const decryptedData = decrypt(encryptedData);
+    res.json({ decryptedData });
+  } else {
+    res.status(400).send('No encrypted data provided');
+  }
+});
+
+// KONFIGURASI PORT
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
